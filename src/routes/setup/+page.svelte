c2e225fa7c76d8a8820e910c4477224a4797be6b
Xylight <james@xylight.us>
2023-04-09 12:10:19 -0700

    feat: Add setup section

--- src/routes/setup/+page.svelte
+++ src/routes/setup/+page.svelte
@@ -1,4 +1,4 @@
-<div class="grid grid-cols-1 gap-4 m-12 w-full">
+<div class="flex flex-col gap-4 m-12 w-full">
     <div
         class="flex flex-col flex-grow col-span-2 gap-4 justify-center items-start mx-4 max-w-2xl md:mx-4"
     >
@@ -9,12 +9,83 @@
             I don't necessarily recommend all of these, I just use them myself.
         </p>
     </div>
-    <section
-        class="grid grid-cols-1 mx-4 max-w-2xl border-l md:grid-cols-2 border-black/10 dark:border-white/5"
-    >
-        <div class="flex-col md:flex-row">
-            <span class="mx-16 ml-8 font-bold">Software</span>
-            <span>Visual Studio Code</span>
+    <span class="ml-4 text-lg font-bold">Software</span>
+    <section class="mx-4 border-l border-black/10 dark:border-white/10">
+        <div class="grid grid-cols-1 gap-2 ml-8 max-w-2xl">
+            <div class="flex justify-between">
+                <span class="font-bold">OS</span>
+                <span>Arch Linux</span>
+            </div>
+            <div class="flex justify-between">
+                <span class="font-bold">Kernel</span>
+                <span>Linux 6.2.10</span>
+            </div>
+            <div class="flex justify-between">
+                <span class="font-bold">DE</span>
+                <span>GNOME 44</span>
+            </div>
+            <div class="flex justify-between">
+                <span class="font-bold">Browser</span>
+                <span>Brave</span>
+            </div>
+            <div class="flex justify-between">
+                <span class="font-bold">Code Editor</span>
+                <span>Visual Studio Code</span>
+            </div>
+        </div>
+    </section>
+    <span class="ml-4 text-lg font-bold">Hardware</span>
+    <section class="mx-4 border-l border-black/10 dark:border-white/10">
+        <div class="grid grid-cols-1 gap-2 ml-8 max-w-2xl">
+            <div class="flex justify-between">
+                <span class="font-bold">CPU</span>
+                <span>Ryzen 5 5600X</span>
+            </div>
+            <div class="flex justify-between">
+                <span class="font-bold">GPU</span>
+                <span>RX 6600 XT</span>
+            </div>
+            <div class="flex justify-between">
+                <span class="font-bold">Memory</span>
+                <span>16GB 2866MHz DDR4</span>
+            </div>
+            <div class="flex justify-between">
+                <span class="font-bold">Motherboard</span>
+                <span>TUF B450M-PLUS</span>
+            </div>
+            <div class="flex justify-between">
+                <span class="font-bold">Drive 1</span>
+                <span>Samsung 870 EVO 1TB</span>
+            </div>
+            <div class="flex justify-between">
+                <span class="font-bold">Drive 2</span>
+                <span>Samsung 860 EVO 500GB</span>
+            </div>
+        </div>
+    </section>
+    <span class="ml-4 text-lg font-bold">Services</span>
+    <section class="mx-4 border-l border-black/10 dark:border-white/10">
+        <div class="grid grid-cols-1 gap-2 ml-8 max-w-2xl">
+            <div class="flex justify-between">
+                <span class="font-bold">Git Provider</span>
+                <span>GitHub</span>
+            </div>
+            <div class="flex justify-between">
+                <span class="font-bold">Hosting Provider</span>
+                <span>Linode</span>
+            </div>
+            <div class="flex justify-between">
+                <span class="font-bold">Mail</span>
+                <span>Proton Mail</span>
+            </div>
+            <div class="flex justify-between">
+                <span class="font-bold">Drive</span>
+                <span>Proton Drive</span>
+            </div>
+            <div class="flex justify-between">
+                <span class="font-bold">VPN</span>
+                <span>Proton VPN</span>
+            </div>
         </div>
     </section>
 </div>
